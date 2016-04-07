f47bc9323c9919bddda203885f16a866e63b36a3
Oscar <racsonp@yahoo.com>
2016-04-06 22:13:09 -0600

    Primer comiit con la template demo

---
+++ racsonpDemo/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using racsonpDemo.Domain;
+using racsonpDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace racsonpDemo.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
